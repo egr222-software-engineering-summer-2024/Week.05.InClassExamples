bb5eae835ec0f94a180320adfa20ff9972a86e20
Larry Clement <lclement@calbaptist.edu>
2024-08-01 14:57:58 -0700

    After completing Point Class v2 Video

--- A/Point/src/Point.java
+++ A/Point/src/Point.java
@@ -1,6 +1,36 @@
+import java.awt.*;
+
 public class Point {
     int x;
     int y;
+
+    public Point(int initialX, int initialY) {
+        x = initialX;
+        y = initialY;
+    }
+
+    public void draw(Graphics g) {
+        g.fillOval(x, y, 3, 3);
+        g.drawString(toString(), x, y);
+    }
+
+    public double distanceFromOrigin() {
+        return Math.sqrt(x*x+y*y);
+    }
+
+    public void translate(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
+
+    public double distanceTo(Point p) {
+        return Math.sqrt((p.x - x)^2 + (p.y - y)^2);
+    }
+
+    public void setLocation(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     public String toString() {
         return "(" + x + ", " + y + ")";
--- A/Point/src/PointClient.java
+++ A/Point/src/PointClient.java
@@ -1,19 +1,36 @@
 // A client using the Custom Point class
+import java.awt.*;
+
 public class PointClient {
 
     public static void main(String[] args) {
-        Point p1 = new Point();
-        p1.x = 10;
-        p1.y = 7;
+        DrawingPanel panel = new DrawingPanel(300, 300);
+        Graphics g = panel.getGraphics();
 
-//        System.out.println("P1 is (" + p1.x + ", " + p1.y + ")");
-        System.out.println("P1 is " + p1);
+        Point p1 = new Point(50, 65);
+//	    p1.x = 50;
+//	    p1.y = 65;
+        Point p2 = new Point(104,83);
+//	    p2.x = 104;
+//	    p2.y = 83;
+        Point p3 = new Point(-5, -1);
+        //p3.x = -5;
 
-//        Point p2 = new Point();
-//        p2.x = 200;
-//        p2.y = 137;
-//
-//        System.out.println("Point 1 is (" + p1.x + ", " + p1.y + ")");
-//        System.out.println("Point 2 is (" + p2.x + ", " + p2.y + ")");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
+
+        // move p2 and then print it
+        p2.translate(2,1);
+
+        System.out.println(p2);
+
+        p1.draw(g);
+        p2.draw(g);
+
+        System.out.println("P1 is " + p1.distanceFromOrigin() + " from the origin.");
+        System.out.println("P2 is " + p2.distanceFromOrigin() + " from the origin.");
+
+        System.out.println("Distance from P1 to P2 is " + p1.distanceTo(p2));
     }
 }
